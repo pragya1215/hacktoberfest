e710d611379e2ad76c7bbbbfe3839a2e6c647cdf
Shagun Goyal <goyalshagun03@gmail.com>
2023-10-07 12:10:12 +0530

    1st commit

--- src/index.js
+++ src/index.js
@@ -2,5 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello World!</h1>, document.getElementById("root")
+  <div>
+  <h1>Hello World!</h1>
+  <h2>Welcome to 1st react app</h2>
+  </div>, document.getElementById("root")
 );
